3977876f303cba67e8a084197a79a299164276eb
mrmurtz <murtaza.s.abidi@gmail.com>
2016-12-15 10:28:08 +0000

    Updated port and hostname

--- app.js
+++ app.js
@@ -4,11 +4,14 @@
 var router = require('./router.js')
 var http = require('http');
 
+var hostname = '127.0.0.1';
+var port = process.env.PORT || 3000;
+
   http.createServer(function(req, res){
     router.home(req, res);
     router.user(req, res);
   }).listen(3000);
-  console.log('Listening on 3000');
+  console.log('Listening on ' + hostname + ":" + port + "/");
 
 // 4. Function the handles reading of files and merge in values
   // read from files
